c0710ac6b3a296f1a3788851f80895e026880e9a
n-kev1n <kevin_@tuta.io>
2022-12-24 23:56:52 +0100

    added initial options

---
+++ lua/user/options.lua
@@ -0,0 +1,10 @@
+-- :help options
+
+vim.opt.showmode = false	-- Disables (-- INSERT --, -- VISUAL --, ...)
+vim.opt.smartcase = true	-- smart case
+vim.opt.smartindent = true	-- make indenting smarter again
+vim.opt.splitbelow = true 	-- force all horizontal splits to go below current window
+vim.opt.splitright = true	-- force all vertical splits to go to the right side of the current window
+vim.opt.swapfile = false	-- creates a swapfile
+vim.opt.expandtab = true	-- convert tabs to spaces
+vim.opt.shiftwidth = 2		-- the number of spaces inserted for each indentation
