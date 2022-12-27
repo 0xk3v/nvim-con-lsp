ef94f423c102353d08cff52ba454abcece12e32b
n-kev1n <kevin_@tuta.io>
2022-12-27 18:41:46 +0100

    plugin module to install packer and other dependencies

---
+++ lua/user/plugins.lua
@@ -0,0 +1,58 @@
+local fn = vim.fn
+
+-- Install Packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Automatically reload neovim when plugin.lua is saved
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Escaping errors in case packer is not installed 
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Open Packer in a Pop-Up Window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Your plugins installed here
+return packer.startup(function(use)
+  
+  -- Packer itself
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+
+  -- Other Plugins
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Plugin Category Here
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
