a59ba659bfa366675a03b80cfa11bb1d53361915
n-kev1n <kevin_@tuta.io>
2022-12-29 16:41:27 +0100

    edited 'modified' icon

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -112,7 +112,7 @@ ins_left {
 ins_left{
   'diff',
   -- Is it me or the symbol for modified us really weird
-  symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+  symbols = { added = ' ', modified = ' ', removed = ' ' },
   diff_color = {
     added = { fg = colors.green },
     modified = { fg = colors.orange },
