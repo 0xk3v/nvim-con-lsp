68c3e89726aa196fe73b7232fb568ee2792a4d15
n-kev1n <kevin_@tuta.io>
2022-12-31 12:07:01 +0100

    changed <ldr>e to open telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -58,12 +58,11 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap(
 	"n",
-	"<leader>f",
+	"<leader>e",
 	"<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",
 	opts
 )
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
-
 
 -- Nvimtree
 keymap("n", "e", ":NvimTreeToggle<CR>", opts)
