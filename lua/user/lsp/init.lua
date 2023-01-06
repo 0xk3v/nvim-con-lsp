b47c96e1bde9d0f8ce4979c502c373e67f06347a
n-kev1n <kevin_@tuta.io>
2023-01-06 23:20:33 +0100

    stylua formatted

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -2,9 +2,9 @@ local status_ok, _ = pcall(require, "lspconfig")
 
 -- Error Prevention
 if not status_ok then
-  return
+	return
 end
 
-require "user.lsp.mason"
+require("user.lsp.mason")
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("user.lsp.null-ls")
