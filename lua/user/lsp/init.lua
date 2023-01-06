15f4b52febce53169a147bc1eee2c8e85f7efd7a
n-kev1n <kevin_@tuta.io>
2023-01-06 12:10:54 +0100

    imported clang_extensions and fidget

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -2,9 +2,18 @@ local status_ok, _ = pcall(require, "lspconfig")
 
 -- Error Prevention
 if not status_ok then
-  return
+	return
 end
 
-require "user.lsp.mason"
+-- Fidget
+
+local status_ok1, fidget = pcall(require, "fidget")
+if not status_ok1 then
+	return
+end
+
+require("user.lsp.mason")
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("user.lsp.null-ls")
+fidget.setup()
+require("user.lsp.clang_extensions")
