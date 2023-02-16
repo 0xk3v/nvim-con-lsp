8192e259075aef95b9dc0d392df42965f2aa3332
n-kev1n <kevin_@tuta.io>
2023-02-16 23:11:57 +0100

    Neo-tree configurations

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,50 @@
+return {
+	-- Configure Neo-tree
+	"nvim-neo-tree/neo-tree.nvim",
+	cmd = "Neotree",
+	keys = {
+		{
+			"e",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+			end,
+			desc = "Explorer NeoTree (root dir)",
+		},
+		{
+			"e",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+			end,
+			desc = "Explorer NeoTree (cwd)",
+		},
+		{ "e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+		{ "E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+	},
+	deactivate = function()
+		vim.cmd([[Neotree close]])
+	end,
+	init = function()
+		vim.g.neo_tree_remove_legacy_commands = 1
+		if vim.fn.argc() == 1 then
+			local stat = vim.loop.fs_stat(vim.fn.argv(0))
+			if stat and stat.type == "directory" then
+				require("neo-tree")
+			end
+		end
+	end,
+	opts = {
+		filesystem = {
+			bind_to_cwd = false,
+			follow_current_file = true,
+		},
+		window = {
+			width = 30,
+			mappings = {
+				["<space>"] = "none",
+				["v"] = "open_vsplit",
+				["l"] = "open",
+				["e"] = "close_window",
+			},
+		},
+	},
+}
