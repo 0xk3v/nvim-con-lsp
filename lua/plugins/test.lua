f95d8762f22eb9af028e2362ba94639475d19133
n-kev1n <kevin_@tuta.io>
2023-09-21 23:35:06 +0200

    added neotest plugins

---
+++ lua/plugins/test.lua
@@ -0,0 +1,64 @@
+return {
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"marilari88/neotest-vitest",
+		},
+    -- stylua: ignore
+		keys = {
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run File",
+			},
+			{
+				"<leader>tT",
+				function()
+					require("neotest").run.run(vim.loop.cwd())
+				end,
+				desc = "Run All Test Files",
+			},
+			{
+				"<leader>tr",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Run Nearest",
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").summary.toggle()
+				end,
+				desc = "Toggle Summary",
+			},
+			{
+				"<leader>to",
+				function()
+          ---@diagnostic disable-next-line: missing-fields
+					require("neotest").output.open({ enter = true, auto_close = true })
+				end,
+				desc = "Show Output",
+			},
+			{
+				"<leader>tO",
+				function()
+					require("neotest").output_panel.toggle()
+				end,
+				desc = "Toggle Output Panel",
+			},
+			{
+				"<leader>tS",
+				function()
+					require("neotest").run.stop()
+				end,
+				desc = "Stop",
+			},
+		},
+		opts = function(_, opts)
+			table.insert(opts.adapters, require("neotest-vitest"))
+		end,
+	},
+}
