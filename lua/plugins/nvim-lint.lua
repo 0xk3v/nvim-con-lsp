d2db5322ff357adcdf8fd022b965d3f4c08251d6
n-kev1n <kevin_@tuta.io>
2023-10-16 21:21:19 +0200

    added js linters

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    event = "LazyFile",
-    opts = {
-      -- Event to trigger linters
-      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-      linters_by_ft = {
-        fish = { "fish" },
-        javascript = {},
-      },
-      -- LazyVim extension to easily override linter options
-      -- or add custom linters.
-      ---@type table<string,table>
-      linters = {
-        -- -- Example of using selene only when a selene.toml file is present
-        -- selene = {
-        --   -- `condition` is another LazyVim extension that allows you to
-        --   -- dynamically enable/disable linters based on the context.
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-      },
-    },
-  }
+	{
+		"mfussenegger/nvim-lint",
+		event = "LazyFile",
+		opts = {
+			-- Event to trigger linters
+			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+			linters_by_ft = {
+				fish = { "fish" },
+				javascript = { "eslint" },
+			},
+			-- LazyVim extension to easily override linter options
+			-- or add custom linters.
+			---@type table<string,table>
+			linters = {
+				-- -- Example of using selene only when a selene.toml file is present
+				-- selene = {
+				--   -- `condition` is another LazyVim extension that allows you to
+				--   -- dynamically enable/disable linters based on the context.
+				--   condition = function(ctx)
+				--     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+				--   end,
+				-- },
+			},
+		},
+	},
 }
