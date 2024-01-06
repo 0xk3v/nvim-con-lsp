5e9a596c7c7037091cb6636c691b5e59f368e107
n-kev1n <kevin_@tuta.io>
2024-01-06 18:16:34 +0100

    added couple color themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,4 +4,23 @@ return {
 		lazy = true,
 		opts = { style = "night" },
 	},
+	{
+		"0xk3v/lavend3r",
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "lavend3r",
+		},
+	},
+	{
+		"tiagovla/tokyodark.nvim",
+		opts = {
+			-- custom options here
+		},
+		config = function(_, opts)
+			require("tokyodark").setup(opts) -- calling setup is optional
+			vim.cmd([[colorscheme tokyodark]])
+		end,
+	},
 }
