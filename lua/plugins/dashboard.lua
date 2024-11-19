39c147aee6def93c3b357dffbe10bf7c8aaa93db
0xk3v <kevin_@tuta.io>
2024-11-19 23:45:03 +0100

    plugin updates

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -15,7 +15,7 @@ return {
 			logo = string.rep("\n", 8) .. logo .. "\n\n"
 
 			local opts = {
-				theme = "doom",
+				theme = "hyper",
 				hide = {
 					-- this is taken care of by lualine
 					-- enabling this messes up the actual laststatus setting after loading a file
